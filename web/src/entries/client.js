17d7fdfebd5e0698aa45fb2db958467c06f8019f
jakwuh <jakwuh@yandex-team.ru>
2017-09-01 23:47:41 +0300

    Add python lang

--- web/src/entries/client.js
+++ web/src/entries/client.js
@@ -4,6 +4,6 @@ hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
 hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
 hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+hljs.registerLanguage('python', require('highlight.js/lib/languages/python'));
 
 hljs.initHighlightingOnLoad();
-
